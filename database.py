62706126feeaf167f73a490e1a4252121ba28ea1
Stover C Mardis <stover.mardis@uvm.edu>
2019-09-29 18:37:49 -0400

    state population

--- database.py
+++ database.py
@@ -3,6 +3,58 @@ import sqlite3
 cities = []
 conn = sqlite3.connect('database.db')
 cur = conn.cursor()
+state_population = {
+    'Alabama': 0,
+    'Alaska': 0,
+    'Arizona': 0,
+    'Arkansas': 0,
+    'California': 0,
+    'Colorado': 0,
+    'Connecticut': 0,
+    'Delaware': 0,
+    'Florida': 0,
+    'Georgia': 0,
+    'Hawaii': 0,
+    'Idaho': 0,
+    'Illinois': 0,
+    'Indiana': 0,
+    'Iowa': 0,
+    'Kansas': 0,
+    'Kentucky': 0,
+    'Louisiana': 0,
+    'Maine': 0,
+    'Maryland': 0,
+    'Massachusetts': 0,
+    'Michigan': 0,
+    'Minnesota': 0,
+    'Mississippi': 0,
+    'Missouri': 0,
+    'Montana': 0,
+    'Nebraska': 0,
+    'Nevada': 0,
+    'New Hampshire': 0,
+    'New Jersey': 0,
+    'New Mexico': 0,
+    'New York': 0,
+    'North Carolina': 0,
+    'North Dakota': 0,
+    'Ohio': 0,
+    'Oklahoma': 0,
+    'Oregon': 0,
+    'Pennsylvania': 0,
+    'Rhode Island': 0,
+    'South Carolina': 0,
+    'South Dakota': 0,
+    'Tennessee': 0,
+    'Texas': 0,
+    'Utah': 0,
+    'Vermont': 0,
+    'Virginia': 0,
+    'Washington': 0,
+    'West Virginia': 0,
+    'Wisconsin': 0,
+    'Wyoming': 0
+}
 
 
 def read_to_database():
@@ -26,6 +78,11 @@ def read_to_database():
     for entry in entries:
         if entry[0] != "city":
             cities.append([entry[0], entry[3], parse_to_int(entry[8]), float(entry[10]), entry[13]])
+            # Construct cities = [[city, state, population, density, timezone]]
+
+    for city in cities:
+        if city[2] != -1:
+            state_population[city[1]] += city[2]
 
 
 def parse_to_int(string):
