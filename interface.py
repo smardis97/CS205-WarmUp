05ff3431511d8eac2c8046cc9bccb037383b4131
Stover C Mardis <stover.mardis@uvm.edu>
2019-10-01 17:38:27 -0400

    fixed issue with multiword city and state names

--- interface.py
+++ interface.py
@@ -14,6 +14,58 @@
 
 from database import *
 
+valid_commands = [
+    'help',
+    'exit',
+    'reload',
+    'population',
+    'density',
+    'timezone',
+    'capital',
+    'city',
+    'state'
+]
+
+
+def validate_and_split(query):
+    divided_query = []
+    next_block = ""
+    last_word = ""
+    multi_word = False
+    for char in query:
+        if char == " ":
+            if not multi_word:
+                if not valid_commands.__contains__(next_block):
+                    multi_word = True
+                    last_word = next_block
+                    next_block = ""
+                else:
+                    divided_query.append(next_block)
+                    next_block = ""
+            else:
+                if not valid_commands.__contains__(next_block):
+                    last_word += " " + next_block
+                    next_block = ""
+                else:
+                    multi_word = False
+                    divided_query.append(last_word)
+                    divided_query.append(next_block)
+                    last_word = ""
+                    next_block = ""
+        elif char == "%":
+            if not multi_word:
+                divided_query.append(next_block)
+                next_block = ""
+            else:
+                last_word += " " + next_block
+                next_block = ""
+                divided_query.append(last_word)
+                last_word = ""
+        else:
+            next_block += char
+
+    return divided_query
+
 
 def print_help():
     # print all available commands
@@ -26,98 +78,92 @@ reinit_database()
 run = True
 success = False
 while run:
-    command = raw_input("-> ")
+    query = validate_and_split(raw_input("-> ") + "%")
     # start with checking if it's the three "basic" commands
-    if command == "exit":
+    if query == "exit":
         run = False
         success = True
-    if command == "help":
+    if query == "help":
         success = True
         print_help()
-    if command == "reload":
+    if query == "reload":
         success = True
         reinit_database()
         print("Database reloaded")
     
     # if it got to this point, it's an actual query of the database
-    query = command.split()
-    if len(query) > 2:
-        if query[0] == "population":
-            if query[1] == "timezone":  # population timezone [x]
-                cur.execute("SELECT population FROM Cities WHERE timezone = ?", (query[2],))
-                rows = cur.fetchall()
-                timezone_pop = 0
-                for row in rows:
-                    if row[0] != -1:
-                        timezone_pop += row[0]
-                print(timezone_pop)
-                success = True
-            if query[1] == "state":  # population state [x]
-                cur.execute("SELECT state_population FROM States WHERE state_name = ?", (query[2],))
-                print(cur.fetchone()[0])
-                success = True
-            if query[1] == "city":  # population city [x] (state x)
-                try:
-                    check_if_exists = query[4]
-                    if query[3] == "state":
-                        cur.execute("SELECT population FROM Cities WHERE state = ? AND city_name = ?", (query[4], query[2],))
-                        print(cur.fetchone()[0])
-                        success = True
-                except IndexError:  # index 3 doesnt exist, no state specified.
-                    cur.execute("SELECT * FROM Cities WHERE city_name = ?", (query[2],))
-                    rows = cur.fetchall()
-                    for row in rows:
-                        if row[2] == -1:
-                            print("%s, %s: population unlisted in database" % (query[2], row[1]))
-                        else:
-                            print("%s, %s: %s" % (query[2], row[1], row[2]))
-                    success = True
-
-        if query[0] == "density":  # density city x (state x)
-            if query[1] == "city":
-                try:
-                    check_if_exists = query[4]
-                    if query[3] == "state":
-                        cur.execute("SELECT density FROM Cities WHERE state = ? AND city_name = ?", (query[4], query[2],))
-                        print(cur.fetchone()[0])
-                        success = True
-                except IndexError:  # index 3 doesnt exist, no state specified.
-                    cur.execute("SELECT * FROM Cities WHERE city_name = ?", (query[2],))
-                    rows = cur.fetchall()
-                    for row in rows:
-                        print("%s, %s: %s" % (query[2], row[1], row[3]))
-                    success = True
-                
-        if query[0] == "timezone":  # timezone city x (state x)
-            if query[1] == "city":
-                try:
-                    check_if_exists = query[4]
-                    if query[3] == "state":
-                        cur.execute("SELECT timezone FROM Cities WHERE state = ? AND city_name = ?", (query[4], query[2],))
-                        print(cur.fetchone()[0])
-                        success = True
-                except IndexError:  # index 3 doesnt exist, no state specified.
-                    cur.execute("SELECT * FROM Cities WHERE city_name = ?", (query[2],))
-                    rows = cur.fetchall()
-                    for row in rows:
-                        print("%s, %s: %s" % (query[2], row[1], row[4]))
-                    success = True
-
-        if query[0] == "state" and query[1] == "city":  # state city x
-            cur.execute("SELECT state FROM Cities WHERE city_name = ?", (query[2],))
+    if query[0] == "population":
+        if query[1] == "timezone":  # population timezone [x]
+            cur.execute("SELECT population FROM Cities WHERE timezone = ?", (query[2],))
             rows = cur.fetchall()
+            timezone_pop = 0
             for row in rows:
-                print("%s, %s" % (query[2], row[0]))
+                if row[0] != -1:
+                    timezone_pop += row[0]
+            print(timezone_pop)
             success = True
-
-        if query[0] == "capital" and query[1] == "state": #capital state [x]
-            cur.execute("SELECT capital_city_id FROM States WHERE state_name = ?", (query[2],))
-            capital_rowid = cur.fetchone()[0]
-            cur.execute("SELECT city_name FROM Cities WHERE rowid = ?", (capital_rowid,))
+        if query[1] == "state":  # population state [x]
+            cur.execute("SELECT state_population FROM States WHERE state_name = ?", (query[2],))
             print(cur.fetchone()[0])
+            success = True
+        if query[1] == "city":  # population city [x] (state x)
+            try:
+                check_if_exists = query[4]
+                if query[3] == "state":
+                    cur.execute("SELECT population FROM Cities WHERE state = ? AND city_name = ?", (query[4], query[2],))
+                    print(cur.fetchone()[0])
+                    success = True
+            except IndexError:  # index 3 doesnt exist, no state specified.
+                cur.execute("SELECT * FROM Cities WHERE city_name = ?", (query[2],))
+                rows = cur.fetchall()
+                for row in rows:
+                    if row[2] == -1:
+                        print("%s, %s: Population Unlisted in Database" % (query[2], row[1]))
+                    else:
+                        print("%s, %s: %s" % (query[2], row[1], row[2]))
+                success = True
+    elif query[0] == "density":  # density city x (state x)
+        if query[1] == "city":
+            try:
+                check_if_exists = query[4]
+                if query[3] == "state":
+                    cur.execute("SELECT density FROM Cities WHERE state = ? AND city_name = ?", (query[4], query[2],))
+                    print(cur.fetchone()[0])
+                    success = True
+            except IndexError:  # index 3 doesnt exist, no state specified.
+                cur.execute("SELECT * FROM Cities WHERE city_name = ?", (query[2],))
+                rows = cur.fetchall()
+                for row in rows:
+                    print("%s, %s: %s" % (query[2], row[1], row[3]))
+                success = True
+    elif query[0] == "timezone":  # timezone city x (state x)
+        if query[1] == "city":
+            try:
+                check_if_exists = query[4]
+                if query[3] == "state":
+                    cur.execute("SELECT timezone FROM Cities WHERE state = ? AND city_name = ?", (query[4], query[2],))
+                    print(cur.fetchone()[0])
+                    success = True
+            except IndexError:  # index 3 doesnt exist, no state specified.
+                cur.execute("SELECT * FROM Cities WHERE city_name = ?", (query[2],))
+                rows = cur.fetchall()
+                for row in rows:
+                    print("%s, %s: %s" % (query[2], row[1], row[4]))
+                success = True
+    elif query[0] == "state" and query[1] == "city":  # state city x
+        cur.execute("SELECT state FROM Cities WHERE city_name = ?", (query[2],))
+        rows = cur.fetchall()
+        for row in rows:
+            print("%s, %s" % (query[2], row[0]))
+        success = True
+    elif query[0] == "capital" and query[1] == "state":  # capital state [x]
+        cur.execute("SELECT capital_city_id FROM States WHERE state_name = ?", (query[2],))
+        capital_rowid = cur.fetchone()[0]
+        cur.execute("SELECT city_name FROM Cities WHERE rowid = ?", (capital_rowid,))
+        print(cur.fetchone()[0])
 
-    if not success: #user failed to input a correct command, help them out
-        if query: #if list is empty this is false
+    if not success:  # user failed to input a correct command, help them out
+        if query:  # if list is empty this is false
             if query[0] == "population":
                 print("population state [x]")
                 print("population timezone [x]")
